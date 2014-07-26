5d96df03f6e595ce6add24e8d6597aaec8fde3ac
Don Lindsay <don.lindsay@gmail.com>
2014-07-26 09:39:21 -0700

    draft mx-mode.el

---
+++ mx-mode.el
@@ -0,0 +1,14 @@
+;; mx-mode.el: A minor mode for Org and OWL2. 
+;; by Don Lindsay 2014
+;; gpl2 boiler
+;; gnu emacs boiler
+;; code examples
+;; hammertime:
+
+(require 'org)
+
+(define-minor-mode mx-mode "A minor mode for Org and OWL2."
+  (message "MX-OWL2: Online")
+					; syntax highlighting
+					; key bindings
+  )
