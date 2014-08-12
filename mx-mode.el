e69bdbc5c6e3ff6d5ca2b0cf92ef7ece6489dfe3
Don Lindsay <don.lindsay@gmail.com>
2014-08-11 17:43:37 -0700

    cleanup cruft files, narrow scope a bit

--- mx-mode.el
+++ mx-mode.el
@@ -18,7 +18,8 @@
 (defun mx:insert-owl2-drawer ()
   "Insert an OWL2 property drawer."
   (interactive)
-  (org-insert-drawer))
+  (org-insert-drawer)
+  )
 
 (defvar mx-owl2-drawer (drawer)
   '((org-insert-drawer . '((mx-org-hidden       . nil)
@@ -37,53 +38,17 @@
 						     )
 		 (org-entry-put-multivalued-property (point)
 						     '((mx-org-property
-						     'mx-predx
-						     '(rdfs-type)))
+							'mx-predx
+							'(rdfs-type)))
 						     )))
 
 
 (defvar mx-triple '((mx-org-headline . rdf:first) . (e1)))
 
-					; (cond 
-(setq mx-triple '(((mx-org-headline . rdf:rest) '(z2))))
-(setq mx-triple '(((z2) rdf:first (e2)) '((z2) rdf:rest . (z3))))
-(setq mx-triple '(((zn) rdf:first (en)) '((zn) rdf:rest . rdf:nil))) 
+(setq mx-triple1 '(((mx-org-headline . rdf:rest) '(z2))))
+(setq mx-triple2 '(((z2) rdf:first (e2)) '((z2) rdf:rest . (z3))))
+(setq mx-triple3 '(((zn) rdf:first (en)) '((zn) rdf:rest . rdf:nil))) 
 
-; equivalence
-(setq mx-eq-axioms '(
-		     (eq-ref   . '(org-entry-put (point) "EQX" "ref"))
-		     (eq-sym   . '(org-entry-put (point) "EQ" "sym"))
-		     (eq-trans . '(org-entry-put (point) "EQ" "trans"))
-		     (eq-rep-s . '(org-entry-put (point) "EQ" "rep-s"))
-		     (eq-rep-p . '(org-entry-put (point) "EQ" "rep-p"))
-		     (eq-rep-o . '(org-entry-put (point) "EQ" "rep-o"))
-		     (eq-diff1 . '(org-entry-put (point) "EQ" "eq-diff1"))
-		     (eq-diff2 . '(org-entry-put (point) "EQ" "eq-diff2"))
-		     (eq-diff3 . '(org-entry-put (point) "EQ" "eq-diff3"))
-		     ))
-
-; property
-(defvar mx-assert-prp-axiom '(
-		      (prp-ap    . '(org-entry-put (point) "PRP" "ap"))
-		      (prp-dom   . '(org-entry-put (point) "PRP" "dom"))
-		      (prp-rng   . '(org-entry-put (point) "PRP" "rng"))
-		      (prp-fp    . '(org-entry-put (point) "PRP" "fp"))
-		      (prp-ifp   . '(org-entry-put (point) "PRP" "ifp"))
-		      (prp-irp   . '(org-entry-put (point) "PRP" "irp"))
-		      (prp-symp  . '(org-entry-put (point) "PRP" "symp"))
-		      (prp-asyp  . '(org-entry-put (point) "PRP" "asymp"))
-		      (prp-trp   . '(org-entry-put (point) "PRP" "trp"))
-		      (prp-spo1  . '(org-entry-put (point) "PRP" "spo1"))
-		      (prp-spo2  . '(org-entry-put (point) "PRP" "spo2"))
-		      (prp-eqp1  . '(org-entry-put (point) "PRP" "eqp1"))
-		      (prp-eqp2  . '(org-entry-put (point) "PRP" "eqp2"))
-		      (prp-pdw   . '(org-entry-put (point) "PRP" "pdw"))
-		      (prp-adp   . '(org-entry-put (point) "PRP" "adp"))
-		      (prp-inv1  . '(org-entry-put (point) "PRP" "inv1"))
-		      (prp-inv2  . '(org-entry-put (point) "PRP" "inv2"))
-		      (prp-key   . '(org-entry-put (point) "PRP" "key"))
-		      (prp-npa1  . '(org-entry-put (point) "PRP" "npa1"))
-		      (prp-npa2  . '(org-entry-put (point) "PRP" "npa2"))))
 
 
 
