9018b6b9fca4cde24d8a256fc7e82f3f0af63029
donlindsay <don.lindsay@gmail.com>
2014-10-12 12:05:27 -0700

    it's really about a sparse tree

--- mx-mode.el
+++ mx-mode.el
@@ -18,6 +18,15 @@
   (message "MX:RL:Active"))
 
 (add-to-list 'org-drawers "MX-AXIOM")
+
+(defvar mx-car ('mx-org-headline . 'rdf:first) . ('e1)))
+
+(defvar mx-cddr ('mx-org-headline . 'rdf:rest) . ('z2)))
+
+(defvar mx-quad ('z2 . 'mx-car ('e2)) (('z2) 'mx-cddr . ('z3))))
+
+(defvar mx-axiom ((('zn) 'mx-car ('en)) '(('zn) 'owl2-rl . nil)))
+
 (defun mx:rl-start (current-buffer)
   "Engage the Rule Language subsystem!"
   (interactive)
@@ -35,21 +44,18 @@
 (defvar mx-drawer 'org-drawer)
 (defcustom mx:rl)
   
-(defun mx:rl-start
-    (setq mx:drawer-quad-prop 'org-custom-properties
-  ("REASONER")
-  ("MX-SUBJ")
-  ("MX-PROP")
-  ("MX-OBJT")
-  ("MX-RULE"))))
+(setq mx:drawer-quad-prop 'org-custom-properties
+      ("REASONER" "MX-SUBJ" "MX-PROP" "MX-OBJT" "MX-RULE"))
 
-(setq org-properties-postprocess-alist '("MX-RULE" .
-  (mx:sparse-tree '((mx:rl)
-  (mx:quad))
-  )
-  )
-  )
-
+(defun mx:sparse-tree tag-exact-match-p
+  "Make a mx:sparse-tree. Like org-sparse-tree, but with a rule."
+  (interactive "P\nmtag:")
+  (org-sparse-tree m
+		   ("mx:rl" . "prp-fp")
+		   ("mx:subj" . "subj")
+		   ("mx:pred" . "pred")
+		   ("mx:obj" . "objt"))
+  
 (defconst mx-triple '((subj . (org-entry-get "MX-SUBJ"))
 		      (prop     . (org-entry-get "MX-PROP"))
 		      (objt     . (org-entry-get "MX-OBJT"))
@@ -65,15 +71,9 @@
   (org-entry-put point-at-bol "object" "ready")
   (message "Writing a triple to the property drawer."))
 
-(defvar mx-car ('mx-org-headline . 'rdf:first) . ('e1)))
-
-(defvar mx-cddr ('mx-org-headline . 'rdf:rest) . ('z2)))
-
-(defvar mx-quad ('z2 . 'mx-car ('e2)) (('z2) 'mx-cddr . ('z3))))
-
-(defvar mx-axiom ((('zn) 'mx-car ('en)) '(('zn) 'owl2-rl . nil)))
 
 
+(provide 'mx-mode)
 
 
 
